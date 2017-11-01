0356b66015be69a297bb04b7277177e9e71237e1
Alenorze <alenorze@gmail.com>
2017-11-01 20:53:31 +0200

    Update pages data

--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from django.contrib.message.views import SuccessMessageMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import View, FormView, CreateView
 
 from newsletter.forms import JoinForm
+from .models import Page
 
 
-class HomeView(CreateView):
+class HomeView(SuccessMessageMixin, CreateView):
     template_name = 'pages/home.html'
     form_class = JoinForm
     success_url = '/'
 
-    def get_success_message(self, cleaned_data):
-        return "Thank you!"
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['page_obj'] = Page.objects.all().order_by("?").first()
+        return context 
 
-    # def form_valid(self, form):
-    #     email = form.cleaned_data.get("email")
-    #     return super(HomeView, self).form_valid(form)
+    def get_success_message(self, cleaned_data):
+        print(cleaned_data)
+        return "Thank you!"

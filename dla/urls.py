0a5758868add8a238b1978139d1c4c1a20b12306
Alenorze <alenorze@gmail.com>
2017-11-02 02:58:08 +0200

    Style update

--- dla/urls.py
+++ dla/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from pages.views import HomeView, PageDetailView
 from newsletter.api.views import JoinCreateAPIView
-
+from pages.views import HomeView, PageDetailView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^(?P<slug>[\w-]+)/$', PageDetailView.as_view(), name='page-detail'),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/email/join/$',JoinCreateAPIView.as_view(), name='email-join'),
+    url(r'^api/email/join/$', JoinCreateAPIView.as_view(), name='email-join'),
+    url(r'^(?P<slug>[\w-]+)/$', PageDetailView.as_view(), name='page-detail'),
+
 ]
